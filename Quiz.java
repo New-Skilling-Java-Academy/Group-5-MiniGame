7e95dedb0dab6a4fbf482eaaf9c5c3deaf57ac4d
Mathias Radke <mathias.radke@telekom.de>
2022-01-17 14:59:23 +0100

    writing Qustion/Answers

---
+++ Quiz.java
@@ -0,0 +1,52 @@
+public class Quiz {
+
+    String[] questions = 	{
+
+            "A person with well-developed abdominal muscles is said to have a what? ",
+            "Which two words traditionally appear onscreen at the termination of a feature film?",
+            "A magnet would most likely attract which of the following?",
+            "Which of these names is not in the title of a Shakespeare play?",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+    };
+
+    String[][] options = 	{
+
+            {"One-pack","Six-pack","12-pack ","family-pack "},
+            {"The End","The Conclusion","The Finish","the TimeOut"},
+            {"Metal","Plastic","Wood","The wrong people"},
+            {"Hamlet","Romeo","Macbeth","Helder"},
+            {" "," "," "," "},
+            {" "," "," "," "},
+            {" "," "," "," "},
+            {" "," "," "," "},
+            {" "," "," "," "},
+            {" "," "," "," "},
+            {" "," "," "," "},
+            {" "," "," "," "},
+            {" "," "," "," "}
+    };
+
+    char[] answers = 		{
+
+            'B',
+            'A',
+            'A',
+            'D',
+            ' ',
+            ' ',
+            ' ',
+            ' ',
+            ' ',
+            ' ',
+            ' ',
+            ' ',
+            ' ',
+    };
+}
