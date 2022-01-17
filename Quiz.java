99c830cd5270831a754e69f6bfdf7ac6f825e69e
Mathias Radke <mathias.radke@telekom.de>
2022-01-17 16:15:26 +0100

    writing Question/Answers

--- Quiz.java
+++ Quiz.java
@@ -6,14 +6,15 @@ public class Quiz {
             "Which two words traditionally appear onscreen at the termination of a feature film?",
             "A magnet would most likely attract which of the following?",
             "Which of these names is not in the title of a Shakespeare play?",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""
+            "Where did Scotch whisky originate?",
+            "In the United States, what is traditionally the proper way to address a judge?",
+            "According to the old saying, love of what is the root of all evil?",
+            "When a tree is cut down, the part that remains in the ground is called what? ",
+            "By definition, a 10-speed bike has 10 what?",
+            "What was the name of the band led by Jim Morrison?",
+            "What is not a car VW name?",
+            "What Supervillian is near Helder?",
+            "Is Java ...?"
     };
 
     String[][] options = 	{
@@ -22,15 +23,15 @@ public class Quiz {
             {"The End","The Conclusion","The Finish","the TimeOut"},
             {"Metal","Plastic","Wood","The wrong people"},
             {"Hamlet","Romeo","Macbeth","Helder"},
-            {" "," "," "," "},
-            {" "," "," "," "},
-            {" "," "," "," "},
-            {" "," "," "," "},
-            {" "," "," "," "},
-            {" "," "," "," "},
-            {" "," "," "," "},
-            {" "," "," "," "},
-            {" "," "," "," "}
+            {"Ireland","The United States","Scotland","Ireland"},
+            {"Your holiness","Your honor","Your eminence","You da man!"},
+            {"Food","Money","Clothing","Reality TV "},
+            {"Hump","Rump ","Stump ","Leftovers"},
+            {"Wheels","Gears","Spokes","Lives"},
+            {"The Doors","The Windows","The Floors","The Roofs"},
+            {"Golf","Jetta","Passat","Bulldog"},
+            {"The Penguin","The Riddler","Two Face","The Joker"},
+            {"Great","Nice","Awesome","a Programming language"}
     };
 
     char[] answers = 		{
@@ -39,14 +40,14 @@ public class Quiz {
             'A',
             'A',
             'D',
+            'C',
+            'B',
+            'B',
+            'C',
+            'B',
+            'A',
+            'D',
             ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
+            ' '
     };
 }
