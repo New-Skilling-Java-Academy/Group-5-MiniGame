7b0317b6793068b83da9e3decfc85bafa7f0a73f
Mathias Radke <mathias.radke@telekom.de>
2022-01-18 12:08:20 +0100

    Answer input & loop

--- QuizGame/QuizApp.java
+++ QuizGame/QuizApp.java
@@ -7,13 +7,15 @@ public class QuizApp {
 
     ArrayList<Question> question = new ArrayList<>();
     Scanner input = new Scanner(System.in);
-
+    int timer = 1;
 
     public QuizApp() {
 
         Question question1 = new Question("A person with well-developed abdominal muscles is said to have a what? ", "One-pack", "Six-pack", "12-pack ", "family-pack ", "Six-pack");
-        Question question2 = new Question("A person with well-developed abdominal muscles is said to have a what? ", "One-pack", "Six-pack", "12-pack ", "family-pack ", "Six-pack");
+        Question question2 = new Question("Which two words traditionally appear onscreen at the termination of a feature film?", "The End", "Six-pack", "12-pack ", "family-pack ", "Six-pack");
         Question question3 = new Question("A person with well-developed abdominal muscles is said to have a what? ", "One-pack", "Six-pack", "12-pack ", "family-pack ", "Six-pack");
+
+
         question.add(question1);
         question.add(question2);
         question.add(question3);
@@ -22,23 +24,81 @@ public class QuizApp {
 
     public void showQuestion() {
         System.out.println();
-        System.out.println("All Question info:");
-        for (Question quest:question) {
-            System.out.println("------------Question " + (question.indexOf(quest)+1) + "---------");
-            System.out.println(question.indexOf(quest)+1 + quest.question);
-            System.out.println(quest.answerA);
-            System.out.println(quest.answerB);
-            System.out.println(quest.answerC);
-            System.out.println(quest.answerD);
+        System.out.println("Quiz");
+        for (Question quest : question) {
+            System.out.println("------------Question " + (question.indexOf(quest) + 1) + "---------");
+            System.out.println(question.indexOf(quest) + 1 + quest.question);
+            System.out.printf(" \t 1: %s - 2: %s - 3: %s - 4: %s ", quest.answerA,  quest.answerB, quest.answerC, quest.answerD);
+
             System.out.println(quest.answerright);
-            System.out.println("------------End----------");
+            System.out.println("--------------------------------------------------------------------------------");
+            if (timer == 10) {
+                printwin(timer);
+            } else {
+                while (timer <= 2) {
+                    System.out.println("Timer is: " + timer);
+                    System.out.print("Answer ");
+                    int choice = input.nextInt();
 
+                    input.nextLine();
+                    switch (choice) {
+                        case 1:
+                            if (quest.answerA.equals(quest.answerright)) {
+                                timer++;
+                                showQuestion();
+                            } else {
+                                printend(timer);
+                                break;
+                            }
+                        case 2:
+                            if (quest.answerB.equals(quest.answerright)) {
+                                timer++;
+                                showQuestion();
+                            } else {
+                                printend(timer);
+                                break;
+                            }
+                        case 3:
+                            if (quest.answerC.equals(quest.answerright)) {
+                                timer++;
+                                showQuestion();
+                            } else {
+                                printend(timer);
+                                break;
+                            }
+                        case 4:
+                            if (quest.answerD.equals(quest.answerright)) {
+                                timer++;
+                                showQuestion();
+                            } else {
+                                printend(timer);
+                                break;
+                            }
+                        default:
+                            System.out.println("Not valid input");
+                            showQuestion();
+                            break;
+                    }
+                }
+            }
         }
-
-        System.out.println();
-
-
     }
 
+
+    public void printend(int timer) {
+        System.out.println();
+        System.out.println("You lose in round " + timer);
+        System.out.println("------------End----------");
+        System.out.println();
+        System.exit(0);
+    }
+
+    public void printwin(int timer) {
+        System.out.println();
+        System.out.println("You win " + timer);
+        System.out.println("------------End----------");
+        System.out.println();
+        System.exit(0);
+    }
 
 }
