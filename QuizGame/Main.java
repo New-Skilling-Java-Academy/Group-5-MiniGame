f7c3f3d2c75b5bb2eb66663c2547c807b08c18b1
Mathias Radke <mathias.radke@telekom.de>
2022-01-18 10:56:08 +0100

    Main and Input

--- QuizGame/Main.java
+++ QuizGame/Main.java
@@ -1,4 +1,9 @@
 package QuizGame;
 
 public class Main {
+
+    public static void main(String[] args) {
+        QuizApp app = new QuizApp();
+        app.showQuestion();
+    }
 }
--- QuizGame/QuizApp.java
+++ QuizGame/QuizApp.java
@@ -1,4 +1,35 @@
 package QuizGame;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class QuizApp {
+
+    ArrayList<Question> question = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
+
+
+    public QuizApp() {
+
+        Question question1 = new Question("A person with well-developed abdominal muscles is said to have a what? ", "One-pack", "Six-pack", "12-pack ", "family-pack ", "Six-pack");
+        Question question2 = new Question("A person with well-developed abdominal muscles is said to have a what? ", "One-pack", "Six-pack", "12-pack ", "family-pack ", "Six-pack");
+        Question question3 = new Question("A person with well-developed abdominal muscles is said to have a what? ", "One-pack", "Six-pack", "12-pack ", "family-pack ", "Six-pack");
+        question.add(question1);
+        question.add(question2);
+        question.add(question3);
+
+    }
+
+    public void showQuestion() {
+        System.out.println();
+        System.out.println("All Question info:");
+        for (Question quest:question) {
+            System.out.println(quest.question + " - " + quest.answerright);
+        }
+        System.out.println("------------End----------");
+        System.out.println();
+
+    }
+
+
 }
